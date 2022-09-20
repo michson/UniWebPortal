8f3bf1c0c1c29a0d239b1e248529a7620200e789
Olaoye Mich <41102062+michson@users.noreply.github.com>
2022-09-20 10:04:01 +0100

    Update Registry.Designer.cs

--- Registry/Registry.Designer.cs
+++ Registry/Registry.Designer.cs
@@ -7,6 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+
 using System;
 using System.Data.Objects;
 using System.Data.Objects.DataClasses;
